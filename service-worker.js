1da6b17e0872db14d92361a05e479b6e0bfb7b03
sondreb <sondre@outlook.com>
2024-12-10 15:22:33 +0000

    Deploying to main from @ block-core/angor@96ae38880ad6667e2b0e3a823871f64ad41ba4df 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: FX/o5lhD */
+/* Manifest version: ow74dvWH */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
